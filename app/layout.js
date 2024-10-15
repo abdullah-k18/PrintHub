4359d1aeeea907eab2b84b6d41c69990f0cfb5bf
abdullah-k18 <abdullahkoraal@gmail.com>
2024-10-15 18:50:53 +0500

    Setting up Google Analytics

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import localFont from "next/font/local";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react"
+import Script from "next/script";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -22,6 +23,23 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <Script
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-DJ0CMQ6XWL"
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-DJ0CMQ6XWL');
+            `,
+          }}
+        />
         <link rel="shortcut icon" href="https://www.svgrepo.com/show/221598/printing-paper-roll.svg" type="image/x-icon" />
       </head>
       <body
